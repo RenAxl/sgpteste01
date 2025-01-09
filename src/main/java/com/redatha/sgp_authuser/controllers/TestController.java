37c133fc40591be413db4bef60efe74b32cb2739
Renan Duarte <renandg.axl@gmail.com>
2025-01-09 09:47:05 -0300

    first commit

---
+++ src/main/java/com/redatha/sgp_authuser/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.redatha.sgp_authuser.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	 @GetMapping("/")
+	    public String testServer() {
+	        return "<html>" +
+	                "<body style=\"text-align: center; font-family: Arial, sans-serif; line-height: 1.5;\">" +
+	                "    <h1>Servidor Funcionando!</h1>" +
+	                "    <pre style=\"font-size: 18px; font-weight: bold;\">*********** WELCOME TO THE SGP! Teste 07 ***********</pre>" +
+	                "</body>" +
+	                "</html>";
+	    }
+	}
---
+++ src/test/java/com/redatha/sgp_authuser/SgpAuthuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redatha.sgp_authuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpAuthuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
